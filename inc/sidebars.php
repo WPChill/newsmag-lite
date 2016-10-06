9c9b4f96d15196c9c8805d7cacbd99b579745add
Cosmin <c0sm1n87@yahoo.co.uk>
2016-10-06 18:34:38 +0300

    am adaugat background attachment fixed

--- inc/sidebars.php
+++ inc/sidebars.php
@@ -8,75 +8,75 @@ if ( function_exists( 'register_sidebar' ) ) {
 			#    Register sidebars
 			#
 			register_sidebar( array(
-				'id'            => 'homepage-slider',
-				'name'          => __( 'Homepage Slider', 'newsmag' ),
-				'description'   => __( 'This sidebar is located at the top of the frontpage and holds the normal slider.', 'newsmag' ),
-				'before_widget' => '<div id="%1$s" class="widget %2$s">',
-				'after_widget'  => '</div>',
-				'before_title'  => '<h3>',
-				'after_title'   => '</h3>',
-			) );
+				                  'id'            => 'homepage-slider',
+				                  'name'          => __( 'Homepage Slider', 'newsmag' ),
+				                  'description'   => __( 'This sidebar is located at the top of the frontpage and holds the normal slider.', 'newsmag' ),
+				                  'before_widget' => '<div id="%1$s" class="widget %2$s">',
+				                  'after_widget'  => '</div>',
+				                  'before_title'  => '<h3>',
+				                  'after_title'   => '</h3>',
+			                  ) );
 
 			register_sidebar( array(
-				'id'            => 'sidebar',
-				'name'          => __( 'Blog Sidebar', 'newsmag' ),
-				'description'   => __( 'This widget is located right side as sidebar. Only seen on the normal post query', 'newsmag' ),
-				'before_widget' => '<div id="%1$s" class="widget %2$s">',
-				'after_widget'  => '</div>',
-				'before_title'  => '<h3>',
-				'after_title'   => '</h3>',
-			) );
+				                  'id'            => 'sidebar',
+				                  'name'          => __( 'Blog Sidebar', 'newsmag' ),
+				                  'description'   => __( 'This widget is located right side as sidebar. Only seen on the normal post query', 'newsmag' ),
+				                  'before_widget' => '<div id="%1$s" class="widget %2$s">',
+				                  'after_widget'  => '</div>',
+				                  'before_title'  => '<h3>',
+				                  'after_title'   => '</h3>',
+			                  ) );
 
 			register_sidebar( array(
-					'id'            => 'content-area',
-					'name'          => __( 'Content Area', 'newsmag' ),
-					'description'   => __( 'This sidebar holds the entire website content', 'newsmag' ),
-					'before_title'  => '<h3 class="widget-title"><span>',
-					'after_title'   => '</span></h3>',
-					'before_widget' => '<div id="%1$s" class="widget %2$s">',
-					'after_widget'  => '</div>'
-				)
+				                  'id'            => 'content-area',
+				                  'name'          => __( 'Content Area', 'newsmag' ),
+				                  'description'   => __( 'This sidebar holds the entire website content', 'newsmag' ),
+				                  'before_title'  => '<h3 class="widget-title"><span>',
+				                  'after_title'   => '</span></h3>',
+				                  'before_widget' => '<div id="%1$s" class="widget %2$s">',
+				                  'after_widget'  => '</div>'
+			                  )
 			);
 
 			register_sidebar( array(
-				'id'            => 'footer-1',
-				'name'          => __( 'Footer 1', 'newsmag' ),
-				'description'   => __( 'This widget is located footer.', 'newsmag' ),
-				'before_widget' => '<div id="%1$s" class="widget %2$s">',
-				'after_widget'  => '</div>',
-				'before_title'  => '<h3 class="widget-title">',
-				'after_title'   => '</h3>',
-			) );
+				                  'id'            => 'footer-1',
+				                  'name'          => __( 'Footer 1', 'newsmag' ),
+				                  'description'   => __( 'This widget is located footer.', 'newsmag' ),
+				                  'before_widget' => '<div id="%1$s" class="widget %2$s">',
+				                  'after_widget'  => '</div>',
+				                  'before_title'  => '<h3 class="widget-title">',
+				                  'after_title'   => '</h3>',
+			                  ) );
 
 			register_sidebar( array(
-				'id'            => 'footer-2',
-				'name'          => __( 'Footer 2', 'newsmag' ),
-				'description'   => __( 'This widget is located footer.', 'newsmag' ),
-				'before_widget' => '<div id="%1$s" class="widget %2$s">',
-				'after_widget'  => '</div>',
-				'before_title'  => '<h3 class="widget-title">',
-				'after_title'   => '</h3>',
-			) );
+				                  'id'            => 'footer-2',
+				                  'name'          => __( 'Footer 2', 'newsmag' ),
+				                  'description'   => __( 'This widget is located footer.', 'newsmag' ),
+				                  'before_widget' => '<div id="%1$s" class="widget %2$s">',
+				                  'after_widget'  => '</div>',
+				                  'before_title'  => '<h3 class="widget-title">',
+				                  'after_title'   => '</h3>',
+			                  ) );
 
 			register_sidebar( array(
-				'id'            => 'footer-3',
-				'name'          => __( 'Footer 3', 'newsmag' ),
-				'description'   => __( 'This widget is located footer.', 'newsmag' ),
-				'before_widget' => '<div id="%1$s" class="widget %2$s">',
-				'after_widget'  => '</div>',
-				'before_title'  => '<h3 class="widget-title">',
-				'after_title'   => '</h3>',
-			) );
+				                  'id'            => 'footer-3',
+				                  'name'          => __( 'Footer 3', 'newsmag' ),
+				                  'description'   => __( 'This widget is located footer.', 'newsmag' ),
+				                  'before_widget' => '<div id="%1$s" class="widget %2$s">',
+				                  'after_widget'  => '</div>',
+				                  'before_title'  => '<h3 class="widget-title">',
+				                  'after_title'   => '</h3>',
+			                  ) );
 
 			register_sidebar( array(
-				'id'            => 'footer-4',
-				'name'          => __( 'Footer 4', 'newsmag' ),
-				'description'   => __( 'This widget is located footer.', 'newsmag' ),
-				'before_widget' => '<div id="%1$s" class="widget %2$s">',
-				'after_widget'  => '</div>',
-				'before_title'  => '<h3 class="widget-title">',
-				'after_title'   => '</h3>',
-			) );
+				                  'id'            => 'footer-4',
+				                  'name'          => __( 'Footer 4', 'newsmag' ),
+				                  'description'   => __( 'This widget is located footer.', 'newsmag' ),
+				                  'before_widget' => '<div id="%1$s" class="widget %2$s">',
+				                  'after_widget'  => '</div>',
+				                  'before_title'  => '<h3 class="widget-title">',
+				                  'after_title'   => '</h3>',
+			                  ) );
 
 		} // function newsmag_register_sidebars end
 
---
+++ template-parts/content-page.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @link    https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Newsmag
+ */
+
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'newsmag-blog-post-layout' ); ?>>
+	<?php
+	$image = '<img class="attachment-newsmag-recent-post-big size-newsmag-recent-post-big wp-post-image" alt="" src="' . esc_url( get_template_directory_uri() . '/assets/images/picture_placeholder.jpg' ) . '" />';
+	if ( has_post_thumbnail() ) {
+		$image = get_the_post_thumbnail( get_the_ID(), 'newsmag-recent-post-big' );
+	}
+	?>
+	<div class="row">
+		<div class="col-sm-4 col-xs-12">
+			<div class="newsmag-image">
+				<a href=" <?php echo esc_url( get_the_permalink() ); ?>">
+					<?php echo wp_kses_post( $image ) ?>
+				</a>
+			</div>
+		</div>
+		<div class="col-sm-8 col-xs-12">
+			<div class="newsmag-title">
+				<h3>
+					<a href="<?php echo esc_url( get_the_permalink() ); ?>"><?php echo esc_html( get_the_title() ); ?></a>
+				</h3>
+				<span class="colored fa fa-clock-o"></span> <?php echo esc_html( get_the_date() ); ?>
+
+			</div>
+			<div class="newsmag-content entry-content">
+				<?php the_content() ?>
+				<span class="newsmag-categories"><?php the_category( ', ' ) ?></span>
+
+			</div>
+		</div>
+	</div>
+
+</article><!-- #post-## -->
