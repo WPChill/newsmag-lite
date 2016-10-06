ef38b2c3a1182f694a78ce967b5d7263a037487c
Cosmin <c0sm1n87@yahoo.co.uk>
2016-10-06 18:36:02 +0300

    content page fix - for now

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,33 +9,19 @@
 
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'newsmag-blog-post-layout' ); ?>>
-	<?php
-	$image = '<img class="attachment-newsmag-recent-post-big size-newsmag-recent-post-big wp-post-image" alt="" src="' . esc_url( get_template_directory_uri() . '/assets/images/picture_placeholder.jpg' ) . '" />';
-	if ( has_post_thumbnail() ) {
-		$image = get_the_post_thumbnail( get_the_ID(), 'newsmag-recent-post-big' );
-	}
-	?>
-	<div class="row">
-		<div class="col-sm-4 col-xs-12">
-			<div class="newsmag-image">
-				<a href=" <?php echo esc_url( get_the_permalink() ); ?>">
-					<?php echo wp_kses_post( $image ) ?>
-				</a>
-			</div>
+
+	<div class="col-xs-12">
+		<div class="newsmag-title">
+			<h3>
+				<a href="<?php echo esc_url( get_the_permalink() ); ?>"><?php echo esc_html( get_the_title() ); ?></a>
+			</h3>
+			<span class="colored fa fa-clock-o"></span> <?php echo esc_html( get_the_date() ); ?>
+
 		</div>
-		<div class="col-sm-8 col-xs-12">
-			<div class="newsmag-title">
-				<h3>
-					<a href="<?php echo esc_url( get_the_permalink() ); ?>"><?php echo esc_html( get_the_title() ); ?></a>
-				</h3>
-				<span class="colored fa fa-clock-o"></span> <?php echo esc_html( get_the_date() ); ?>
+		<div class="newsmag-content entry-content">
+			<?php the_content() ?>
+			<span class="newsmag-categories"><?php the_category( ', ' ) ?></span>
 
-			</div>
-			<div class="newsmag-content entry-content">
-				<?php the_content() ?>
-				<span class="newsmag-categories"><?php the_category( ', ' ) ?></span>
-
-			</div>
 		</div>
 	</div>
 
