00b197a8bba6f41c59df05ffa16d5d4ebb4aab1c
Cosmin <c0sm1n87@yahoo.co.uk>
2016-10-07 12:11:02 +0300

    #111

--- inc/widgets/homepage_builder/layouts/recent.php
+++ inc/widgets/homepage_builder/layouts/recent.php
@@ -9,22 +9,22 @@ if ( $posts->have_posts() ): ?>
 
 		<?php
 		while ( $posts->have_posts() ) : $posts->the_post();
-			$image = '<img class="attachment-newsmag-recent-post-big size-newsmag-recent-post-big wp-post-image" alt="" src="' . esc_url( get_template_directory_uri() . '/assets/images/picture_placeholder.jpg' ) . ' " />';
+			$image = '<img class="attachment-newsmag-recent-post-list-image size-newsmag-recent-post-list-image wp-post-image" alt="" src="' . esc_url( get_template_directory_uri() . '/assets/images/picture_placeholder_list.jpg' ) . ' " />';
 			if ( has_post_thumbnail() ) {
-				$image = get_the_post_thumbnail( get_the_ID(), 'newsmag-recent-post-big' );
+				$image = get_the_post_thumbnail( get_the_ID(), 'newsmag-recent-post-list-image' );
 			}
 			?>
 
 			<div class="newsmag-blog-post-layout-b">
 				<div class="row">
-					<div class="col-sm-5 col-xs-12">
+					<div class="col-sm-3 col-xs-12">
 						<div class="newsmag-image">
 							<a href="<?php echo get_the_permalink(); ?>">
 								<?php echo wp_kses_post( $image ) ?>
 							</a>
 						</div>
 					</div>
-					<div class="col-sm-7 col-xs-12">
+					<div class="col-sm-9 col-xs-12">
 						<div class="newsmag-title">
 							<h4>
 								<a href="<?php echo esc_url( get_the_permalink() ); ?>"><?php echo wp_trim_words( get_the_title(), 15 ); ?></a>
