1bd6b1a142e9e5a18b522b9908478d19d8f426ce
Cosmin <c0sm1n87@yahoo.co.uk>
2016-10-07 11:25:57 +0300

    #115 -I'm a simple man ..  I see issue, i fix issue

--- inc/sidebars.php
+++ inc/sidebars.php
@@ -10,7 +10,7 @@ if ( function_exists( 'register_sidebar' ) ) {
 			register_sidebar( array(
 				                  'id'            => 'homepage-slider',
 				                  'name'          => __( 'Homepage Slider', 'newsmag' ),
-				                  'description'   => __( 'This sidebar is located at the top of the frontpage and holds the normal slider.', 'newsmag' ),
+				                  'description'   => __( 'This sidebar holds the header sidebar area on the homepage.', 'newsmag' ),
 				                  'before_widget' => '<div id="%1$s" class="widget %2$s">',
 				                  'after_widget'  => '</div>',
 				                  'before_title'  => '<h3>',
@@ -20,7 +20,7 @@ if ( function_exists( 'register_sidebar' ) ) {
 			register_sidebar( array(
 				                  'id'            => 'sidebar',
 				                  'name'          => __( 'Blog Sidebar', 'newsmag' ),
-				                  'description'   => __( 'This widget is located right side as sidebar. Only seen on the normal post query', 'newsmag' ),
+				                  'description'   => __( 'The sidebar holds the entire homepage content, place "Newsmag - Homepage builder" widgets here, please consider the column arrangement.', 'newsmag' ),
 				                  'before_widget' => '<div id="%1$s" class="widget %2$s">',
 				                  'after_widget'  => '</div>',
 				                  'before_title'  => '<h3>',
