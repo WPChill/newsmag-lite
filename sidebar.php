cb1905d48e14dbe5d72758375dd296011afe207d
Cosmin <c0sm1n87@yahoo.co.uk>
2016-10-31 10:25:02 +0200

    #200 - ceva margin fix

--- sidebar.php
+++ sidebar.php
@@ -10,9 +10,10 @@
 if ( ! is_active_sidebar( 'sidebar' ) ) {
 	return;
 }
+$breadcrumbs_enabled = get_theme_mod( 'newsmag_enable_post_breadcrumbs', true );
 ?>
 
-<aside id="secondary" class="widget-area col-lg-4 col-md-4 col-sm-4 newsmag-sidebar hidden-xs" role="complementary">
+<aside id="secondary" class="<?php echo $breadcrumbs_enabled ? '' : 'newsmag-margin-top';?> widget-area col-lg-4 col-md-4 col-sm-4 newsmag-sidebar hidden-xs" role="complementary">
 	<div class="newsmag-blog-sidebar">
 		<?php dynamic_sidebar( 'sidebar' ); ?>
 	</div>
