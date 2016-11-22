8329450007aa5cf3f55a147059fc074277178f65
Cosmin <c0sm1n87@yahoo.co.uk>
2016-11-22 15:09:19 +0200

    am scos fade`ul de pe go top button

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -120,14 +120,10 @@ var Newsmag = {
 
 	initGoToTop: function ($) {
 		var offset = 300,
-				offset_opacity = 1200,
 				scroll_top_duration = 700,
 				$back_to_top = $('#back-to-top');
 		jQuery(window).scroll(function () {
-			( jQuery(this).scrollTop() > offset ) ? $back_to_top.addClass('back-to-top-is-visible') : $back_to_top.removeClass('back-to-top-is-visible back-to-top-fade-out');
-			if ( jQuery(this).scrollTop() > offset_opacity ) {
-				$back_to_top.addClass('back-to-top-fade-out');
-			}
+			( jQuery(this).scrollTop() > offset ) ? $back_to_top.addClass('back-to-top-is-visible') : $back_to_top.removeClass('back-to-top-is-visible');
 		});
 		$back_to_top.on('click', function (event) {
 			event.preventDefault();
