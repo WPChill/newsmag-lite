0cb251c4cae77f2b1c5488df532a88a2722c7e72
George Ciobanu <george.ciobanu91@gmail.com>
2019-09-03 13:08:14 +0300

    small fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -160,7 +160,7 @@ module.exports = function (grunt) {
 		checktextdomain: {
 			standard: {
 				options: {
-					text_domain       : [ 'newsmag' ], //Specify allowed domain(s)
+					text_domain       : [ 'newsmag', 'epsilon-framework' ], //Specify allowed domain(s)
 					create_report_file: "true",
 					keywords          : [ //List keyword specifications
 						'__:1,2d',
